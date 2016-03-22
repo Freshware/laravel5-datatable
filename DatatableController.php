c05fb1b5d079a85c5baf779296d09c1191524cfd
SeiyaJapon <info@seiyajapon.com>
2016-03-22 12:37:32 +0100

    New Controller let change route and datatable template

--- DatatableController.php
+++ DatatableController.php
@@ -12,22 +12,26 @@ class DatatableController extends Controller
     /**
      * Para mostrar datatables.
      *
+     * @param   string      $template       [cambiamos la plantilla por defecto por otra a nuestro gusto]
      * @param   string      $dt_id          [el id de la tabla que se convertira en datatable]
      * @param   Collection  $values         [representa la colección de valores que se han de incorporar a la tabla]
      * @param   array       $array_values   [array que contiene los atributos/columnas a mostrar]
+     * @param   string      $route          [ruta del controlador al que saltar]
      * @param   string      $link           [dirección a la que saltar en el primer parámetro, por defecto 'edit']
      * @param   boolean     $search         [true = se muestran inputs de busqueda en cada columna (pierde traducción)]
      * @return vista HTML
      */
-    public function datatable($dt_id, $values, $array_values, $link = 'edit', $search = false)
+    public function datatable($dt_id, $values, $array_values, $link = 'edit', $route = NULL, $search = false, $template = 'datatable.datatable')
     {
-        $view = \View::make('datatable.datatable', [
-                                                        'values'        => $values,
-                                                        'datatable_id'  => $dt_id,
-                                                        'array_values'  => $array_values,
-                                                        'link'          => $link,
-                                                        'search'        => $search
-                                                    ]);
+        $view = \View::make($template, [
+                                            'values'        => $values,
+                                            'datatable_id'  => $dt_id,
+                                            'array_values'  => $array_values,
+                                            'route'         => $route,
+                                            'link'          => $link,
+                                            'search'        => $search,
+                                            'template'      => $template
+                                        ]);
         $contents = $view->render();
 
         return  $contents;
